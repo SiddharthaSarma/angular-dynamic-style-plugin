64e2abea2061572e4758d1ddce371dc4075aa43e
siddharthasarma <sidduinevitable@gmail.com>
2019-03-21 17:22:10 +0530

    add library to app to check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularDynamicStylePluginLibModule } from 'angular-dynamic-style-plugin-lib';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularDynamicStylePluginLibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
