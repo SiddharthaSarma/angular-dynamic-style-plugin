dd12a1d088e56c18019ad8e9ceb76c3fad30168a
siddharthasarma <sidduinevitable@gmail.com>
2019-03-21 18:11:53 +0530

    update package.json with scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularDynamicStylePluginLibModule } from 'angular-dynamic-style-plugin-lib';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AngularDynamicStylePluginLibModule, FormsModule],
+  imports: [BrowserModule, AngularDynamicStylePluginLibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
